1c673dc8e0d69a74d60fb0bfe5ff9d8f303598c5
kirmiziben10 <tigiy20032003@gmail.com>
2026-02-08 00:19:19 +0300

    Add dark-mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import './App.css'
-import Sidebar from "./Components/Sidebar"
+import { useState } from "react";
+import "./index.css";
+import { Sun, Moon } from "lucide-react";
 
 function App() {
-
+  const [theme, setTheme] = useState("");
+  function manageTheme() {
+    theme === "dark" ? setTheme("") : setTheme("dark");
+  }
 
   return (
-    <>
-      <Sidebar />
-    </>
-  )
+    <div
+      className={
+        "bg-zinc-50 dark:bg-zinc-950 grid place-items-center h-screen w-f " +
+        theme
+      }
+    >
+      <div className="bg-zinc-300 dark:bg-zinc-700 flex place-items-center rounded p-1">
+          <button className="p-1 rounded hover:bg-zinc-400" onClick={manageTheme}>
+            {theme === "dark" ? (
+              <Sun color="white" size={24} />
+            ) : (
+              <Moon color="black" size={24} />
+            )}
+          </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
